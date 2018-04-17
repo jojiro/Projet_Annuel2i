9acdc8f151d4008a7a6ed0c54e8b3ca9aa62b26a
Asianono <kenjiyoroba@yahoo.fr>
2018-04-17 13:51:14 +0200

    pull sac mere

---
+++ Web/connexion.php
@@ -0,0 +1,37 @@
+<?php
+	session_start();
+
+	require "header.php";
+
+?>
+
+
+<section id="contact-block">
+
+	<div class="row">
+				<div class="col-lg-12">
+					<h1 id="contact-signup-title">Connexion</h1>
+					<br><br>
+				</div>
+			</div>
+
+
+		<section>
+			<form method="post" action="connexion.php">
+				<input class="champ" type="email" name="email" placeholder="Votre email" required="required"><br>
+				<input class="champ" type="password" name="password" placeholder="Votre mot de passe" required="required"><br>
+				<input class="champ" id="bt5" type="submit" value="Se connecter">
+			</form>
+</section>
+
+
+		</section>
+
+<?php
+
+require "footer.php";
+
+?>
+		
+	
+</html>
---
+++ Web/index.php
@@ -0,0 +1,405 @@
+<?php
+	require "header.php";
+?>
+	<section id="fh5co-home" data-section="home" style="background-image: url(images/full_image_3.jpg);" data-stellar-background-ratio="0.5">
+		<div class="gradient"></div>
+		<div class="container">
+			<div class="text-wrap">
+				<div class="text-inner">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2">
+							<h1 class="to-animate">Workin'Space</h1>
+							<h2 class="to-animate">Des solutions simple et rapide pour travailler dans les meilleures conditions </h2>
+							<div class="call-to-action">
+								<a href="connexion" class="demo to-animate">Se connecter</a>
+								<a href="inscription" class="download to-animate">S'inscrire</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="fh5co-explore" data-section="explore">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 section-heading text-center">
+					<h2 class="to-animate">Explorez nos espaces</h2>
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 subtext to-animate">
+							<h3>Visitez une maquette en 3D des locaux que vous pouvez louer!</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="fh5co-explore">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-push-5 to-animate-2">
+						<img class="img-responsive" src="images/work_1.png" alt="work">
+					</div>
+					<div class="col-md-4 col-md-pull-8 to-animate-2">
+						<div class="mt">
+							<h3>Intégration WebGl</h3>
+							
+							<ul class="list-nav">
+								<li><i class="icon-check2"></i>Far far away, behind the word</li>
+								<li><i class="icon-check2"></i>There live the blind texts</li>
+								<li><i class="icon-check2"></i>Separated they live in bookmarksgrove</li>
+								<li><i class="icon-check2"></i>Semantics a large language ocean</li>
+								<li><i class="icon-check2"></i>A small river named Duden</li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		
+	</section>
+	<section id="fh5co-testimony" data-section="testimony">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 to-animate">
+					<div class="wrap-testimony">
+						<div class="owl-carousel-fullwidth">
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<figure>
+										<img src="images/person2.jpg" alt="user">
+									</figure>
+									<blockquote>
+										<p>"La solution qui m'a été proposée par Workin'Space m'as permis de me consacrer corps et âme sur mon projet et d'atteindre des niveaux de concentration et de productivité seulement possible dans un tel environnement."</p>
+									</blockquote>
+									<span>Xavier Niel, via <a href="#" class="twitter">Twitter</a></span>
+								</div>
+							</div>
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<figure>
+										<img src="images/person3.jpg" alt="user">
+									</figure>
+									<blockquote>
+										<p>"Simplement la meilleure solution pour travailler loin des distractions du quotidien."</p>
+									</blockquote>
+									<span>Nicolas Boisan, via <a href="#" class="twitter">Twitter</a></span>
+								</div>
+							</div>
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<figure>
+										<img src="images/person2.jpg" alt="user">
+									</figure>
+									<blockquote>
+										<p>"Grâce à Workin'Space j'ai pû lancer ma start up dans un environnement favorable et louer des équipment qui m'ont permis d'avancer trés rapidement ."</p>
+									</blockquote>
+									<span>Pierre Belmart, via <a href="#" class="twitter">Twitter</a></span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+	
+	<section id="fh5co-pricing" data-section="pricing">
+		<div class="fh5co-pricing">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate">Des solutions pour tout le monde</h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate">Que vous soyez freelance en recherche d'un environnment studieux ou chef d'équipe cherchant du matériel adéquat à ses besoins, nous avons la solution pour vous. </h3>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="pricing">
+						<div class="col-md-3">
+							<div class="price-box to-animate-2">
+								<h2 class="pricing-plan">Formule à l'heure</h2>
+								<div class="price"><sup class="currency">€</sup>1<small>/heure</small></div>
+								<p>Payez en fonction du temps dont vous avez réellement besoin.</p>
+								<a href="#" class="btn btn-select-plan btn-sm">Choisir cette formule</a>
+							</div>
+						</div>
+
+
+						<div class="col-md-3">
+							<div class="price-box to-animate-2 popular">
+								<h2 class="pricing-plan pricing-plan-offer">Formule au mois <span>Illimité</span></h2>
+								<div class="price"><sup class="currency">€</sup>80<small>/mois</small></div>
+								<p>La formule idéale pour profiter de tout les atouts que Workin'Space mets à votre disposition. </p>
+								<a href="#" class="btn btn-select-plan btn-sm">Choisir cette formule</a>
+							</div>
+						</div>
+
+						<div class="col-md-3">
+							<div class="price-box to-animate-2 popular">
+								<h2 class="pricing-plan pricing-plan-offer">Formule 6 mois <span>2 mois offert</span></h2>
+								<div class="price"><sup class="currency">€</sup>320<small>/mois</small></div>
+								<p>La formule parfaite pour donner vie à vos plus gros projets. </p>
+								<a href="#" class="btn btn-select-plan btn-sm">Choisir cette formule</a>
+							</div>
+						</div>
+
+						<div class="col-md-3">
+							<div class="price-box to-animate-2">
+								<h2 class="pricing-plan">Les services</h2>
+								<p>IMPRESSION <br>Noir et Blanc: 0.07€<br>Couleur: 0.30€<br>Scan: Gratuit<br><br>SALLE DE RÉUNION<br>5€/heure<br><br>BOISSON ILLIMITÉ<br>Café, Soda, Eau... </p>
+							</div>
+						</div>
+					</div>
+				</div>
+			<!--
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 to-animate">
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. <a href="#">Learn More</a></p>
+					</div>
+				</div>
+			-->
+
+			</div>
+		</div>
+	</section>
+
+	<section id="fh5co-services" data-section="services">
+		<div class="fh5co-services">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate">Nous proposons des services...</h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate"></h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="box-services">
+							<i class="icon-chemistry to-animate-2"></i>
+							<div class="fh5co-post to-animate">
+								<h3>Services à disposition</h3>
+								<p>Que ce soit un besoin de matériel informatique ou un manque de boisson cafféiné, Workin'Space est équipé pour répondre à vos demandes.</p>
+							</div>
+						</div>
+					</div>
+						<div class="col-sm-4">
+
+							<div class="box-services">
+								<i class="icon-energy to-animate-2"></i>
+								<div class="fh5co-post to-animate">
+									<h3>Connexion professionnelle</h3>
+									<p>Profitez d'une connexion internet de niveau professionnel pour un travail sans interruption et sécurisé. </p>
+								</div>
+							</div>
+					</div>
+					<!--
+					<div class="col-md-4">
+						<div class="box-services">
+							<i class="icon-trophy to-animate-2"></i>
+							<div class="fh5co-post to-animate">
+								<h3>Award-winning Landing Page</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in.</p>
+							</div>
+						</div>
+
+						<div class="box-services">
+							<i class="icon-paper-plane to-animate-2"></i>
+							<div class="fh5co-post to-animate">
+								<h3>Sleek and Smooth Animation</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in.</p>
+							</div>
+						</div>
+					</div>
+				-->
+					<div class="col-sm-4">
+						<div class="box-services">
+							<i class="icon-people to-animate-2"></i>
+							<div class="fh5co-post to-animate">
+								<h3>Un environnement studieux</h3>
+								<p>Cotoyez des personnes ayant les mêmes motivations, un bon moyens de créer des contacts. </p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+				<!--
+				<div class="call-to-action text-center to-animate"><a href="#" class="btn btn-learn">Learn More</a></div>
+			-->
+			</div>
+		</div>
+	</section>	
+<!--
+	<section id="fh5co-team" data-section="team">
+		<div class="fh5co-team">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate">Meet The Team</h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove. </h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="team-box text-center to-animate-2">
+							<div class="user"><img class="img-reponsive" src="images/person4.jpg" alt="Roger Garfield"></div>
+							<h3>Roger Garfield</h3>
+							<span class="position">Co-Founder, Lead Developer</span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<ul class="social-media">
+								<li><a href="#" class="facebook"><i class="icon-facebook"></i></a></li>
+								<li><a href="#" class="twitter"><i class="icon-twitter"></i></a></li>
+								<li><a href="#" class="dribbble"><i class="icon-dribbble"></i></a></li>
+								<li><a href="#" class="codepen"><i class="icon-codepen"></i></a></li>
+								<li><a href="#" class="github"><i class="icon-github-alt"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="team-box text-center to-animate-2">
+							<div class="user"><img class="img-reponsive" src="images/person2.jpg" alt="Roger Garfield"></div>
+							<h3>Kevin Steve</h3>
+							<span class="position">Co-Founder, Product Designer</span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<ul class="social-media">
+								<li><a href="#" class="facebook"><i class="icon-facebook"></i></a></li>
+								<li><a href="#" class="twitter"><i class="icon-twitter"></i></a></li>
+								<li><a href="#" class="dribbble"><i class="icon-dribbble"></i></a></li>
+								<li><a href="#" class="codepen"><i class="icon-codepen"></i></a></li>
+								<li><a href="#" class="github"><i class="icon-github-alt"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="team-box text-center to-animate-2">
+							<div class="user"><img class="img-reponsive" src="images/person3.jpg" alt="Roger Garfield"></div>
+							<h3>Ross Standford</h3>
+							<span class="position">Full Stack Developer</span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<ul class="social-media">
+								<li><a href="#" class="facebook"><i class="icon-facebook"></i></a></li>
+								<li><a href="#" class="twitter"><i class="icon-twitter"></i></a></li>
+								<li><a href="#" class="dribbble"><i class="icon-dribbble"></i></a></li>
+								<li><a href="#" class="codepen"><i class="icon-codepen"></i></a></li>
+								<li><a href="#" class="github"><i class="icon-github-alt"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+-->
+
+	<section id="fh5co-faq" data-section="faq">
+		<div class="fh5co-faq">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate">Questions communes</h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate">Tout ce que vous devez savoir avant de commencer</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="box-faq to-animate-2">
+							<h3>Qu'est-ce que Workin'Space?</h3>
+							<p>Workin'Space est une solution de location de d'espaces de travail. Nous vous fournissons espaces, matériel, boissons et connexion internet haut débit afin de vous permettre une ambiance de travail optimale.</p>
+						</div>
+						<div class="box-faq to-animate-2">
+							<h3>Quel type d'équipement fournissez vous?</h3>
+							<p>Nous fournissons: ordinateurs portables, imprimante, projecteur, chargeurs... </p>
+						</div>
+						
+					</div>
+
+					<div class="col-md-6">
+						<div class="box-faq to-animate-2">
+							<h3>Comment puis-je profiter de vos services?</h3>
+							<p>Pour profiter de toutes les solutons Workin'Space, vous devez vous connecter (ou créer un compte ) et choisir une de nos formules afn de réserver vos locaux.</p>
+						</div>
+						<div class="box-faq to-animate-2">
+							<h3>Combien de personnes peuvent venir travailler avec moi?</h3>
+							<p>Nous proposons plusieurs taille de locaux et vous pouvez ainsi collaborer aves 8 personnes dans nos locaux les plus large.</p>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<hr>
+<!--
+	<section id="fh5co-trusted" data-section="trusted">
+		<div class="fh5co-trusted">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate">Trusted By</h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate">We’re trusted by these popular companies</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					 <div class="col-md-2 col-sm-3 col-xs-6 col-sm-offset-0 col-md-offset-1">
+					 	<div class="partner-logo to-animate-2">
+					 		<img src="images/logo1.png" alt="Partners" class="img-responsive">
+					 	</div>
+					 </div>
+				    <div class="col-md-2 col-sm-3 col-xs-6">
+				    	<div class="partner-logo to-animate-2">
+				    		<img src="images/logo2.png" alt="Partners" class="img-responsive">
+						</div>
+				    </div>
+				    <div class="col-md-2 col-sm-3 col-xs-6">
+				    	<div class="partner-logo to-animate-2">
+				    		<img src="images/logo3.png" alt="Partners" class="img-responsive">
+				    	</div>
+				    </div>
+				    <div class="col-md-2 col-sm-3 col-xs-6">
+				    	<div class="partner-logo to-animate-2">
+				    		<img src="images/logo4.png" alt="Partners" class="img-responsive">
+				    	</div>
+				    </div>
+				    <div class="col-md-2 col-sm-12 col-xs-12">
+				    	<div class="partner-logo to-animate-2">
+				    		<img src="images/logo5.png" alt="Partners" class="img-responsive">
+				    	</div>
+				    </div>
+				</div>
+			</div>
+		</div>
+	</section>
+-->
+
+
+<?php
+	require "footer.php";
+?>
